edee009c12a4dfd340291a07405f110feefd1edb
greigm <greig@hamiltron.net>
2014-02-20 12:50:28 +1300

    Some bugfixes

--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -1,7 +1,7 @@
 module BeersHelper
 
-	def beer_price(beer, serving_size)
-		price = ( (beer.price * Beer.markup("tap")) / (beer.format.size / serving_size) ).round(1)
+	def beer_price(beer, serving_size, method)
+		price = ( (beer.price * Beer.markup(:method)) / (beer.format.size / serving_size) ).round(1)
 		number_to_currency(price, precision: 2)
 	end
 
