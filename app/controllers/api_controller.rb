80f21526430cf928b5aad11ec5728a12d3cea036
greigm <greig@hamiltron.net>
2014-02-20 10:57:54 +1300

    Don't abuse #match in the routes, and inside put the API in it's own namespace

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,3 @@
-class ApiController < ApplicationController
-
-	def index
-	end
-
-	def beers
-		@beers = Beer.ontap
-		render :json=>@beers, :except => :id
-	end
+class ApiController < ActionController::Base
 
 end
