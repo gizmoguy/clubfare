b43905340ec3f77285fe89cca838476667cf824c
greigm <greig@hamiltron.net>
2014-03-03 22:08:33 +1300

    Add JS error handling and fix spurious </ul> in beer list

--- app/assets/javascripts/dash.js
+++ app/assets/javascripts/dash.js
@@ -1,11 +1,18 @@
 var ready;
 ready = function() {
-  setTimeout(function() {
-    var source = new EventSource('/streaming');
-    source.addEventListener('refresh', function(e) {
-      $.get('/dash.js');
-    });
-  }, 1);
+	setTimeout(function() {
+		var source = new EventSource('/streaming');
+		source.addEventListener('refresh', function(e) {
+			$.get('/dash.js');
+    	});
+		source.onerror = function() {
+			console.log('Error!');
+			console.log(arguments);
+		};
+		setInterval(function() {
+			$('#event_source_status').html(source.readyState);
+		}, 1000);
+  	}, 1);
 };
 
 $(document).ready(ready);
