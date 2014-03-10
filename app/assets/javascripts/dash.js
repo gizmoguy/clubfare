93d4d537a9545d1e6af1dabb192f484397275b68
greigm <greig@hamiltron.net>
2014-03-10 14:12:54 +1300

    Fix javascript bug

--- app/assets/javascripts/dash.js
+++ app/assets/javascripts/dash.js
@@ -3,7 +3,29 @@ ready = function() {
 	var source = new EventSource('/streaming');
 	source.addEventListener('refresh', function(e) {
 		$.get('/dash.js');
-   	});
+		setInterval(function() {
+			switch (source.readyState) {
+				case EventSource.CONNECTING:
+					// do nothing yet
+					$('#event_source_status').html(source.readyState);
+					break;
+				case EventSource.OPEN:
+					// this is normal
+					$('#event_source_status').html(source.readyState);
+					break;
+				case EventSource.CLOSED:
+					// Start a new listener
+					$('#event_source_status').html(source.readyState);
+					source.addEventListener('refresh', function(e) {
+						$.get('/dash.js');
+					});
+					break;
+				default:
+					// this can't happen
+					break;
+			}
+		}, 1000);
+	});
 };
 
 $(document).ready(ready);
