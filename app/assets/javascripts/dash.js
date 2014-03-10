25c36c0324d8ac77bf821348b8ab08361266cecb
greigm <greig@hamiltron.net>
2014-03-10 14:54:44 +1300

    Fix javascript bug

--- app/assets/javascripts/dash.js
+++ app/assets/javascripts/dash.js
@@ -1,32 +1,38 @@
-var ready;
-ready = function() {
-	var source = new EventSource('/streaming');
-	source.addEventListener('refresh', function(e) {
-		$.get('/dash.js');
-		setInterval(function() {
-			switch (source.readyState) {
-				case EventSource.CONNECTING:
-					// do nothing yet
-					$('#event_source_status').html(source.readyState);
-					break;
-				case EventSource.OPEN:
-					// this is normal
-					$('#event_source_status').html(source.readyState);
-					break;
-				case EventSource.CLOSED:
-					// Start a new listener
-					$('#event_source_status').html(source.readyState);
-					source.addEventListener('refresh', function(e) {
-						$.get('/dash.js');
-					});
-					break;
-				default:
-					// this can't happen
-					break;
-			}
-		}, 1000);
-	});
+var source;
+var ready = function() {
+	
+	source = new EventSource('/streaming');
+	source.addEventListener('refresh', updateDash); 
+
+	setInterval(function() {
+		switch (source.readyState) {
+			case EventSource.CONNECTING:
+				// do nothing yet
+				$('#event_source_status').html(source.readyState);
+				break;
+				
+			case EventSource.OPEN:
+				// this is normal
+				$('#event_source_status').html(source.readyState);
+				break;
+				
+			case EventSource.CLOSED:
+				// Start a new listener
+				$('#event_source_status').html(source.readyState);
+				source = new EventSource('/streaming');
+				source.addEventListener('refresh', updateDash);
+				break;
+				
+			default:
+				// this can't happen
+				break; // why break when there's nothing below it?
+		}
+	}, 1000);
 };
+
+updateDash = function(e) {
+	$.get('/dash.js');
+}
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
