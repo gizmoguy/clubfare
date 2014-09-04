1a212fa1825d81cb1ea6b6704cc065d3bb597247
greigm <greig@hamiltron.net>
2014-09-04 15:44:49 +1200

    Minor bugfix

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,5 @@ class Location < ActiveRecord::Base
 	has_many :beers
 
 	validates :name, length: { maximum: 100 }, presence: true
-	validates :location, length: { maximum: 100 }, presence: true
+	validates :status, length: { maximum: 100 }, presence: true
 end
