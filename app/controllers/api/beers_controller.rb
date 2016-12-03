df34c2a217661b4e0cd28ea7d10e7777177b8d1d
Brad Cowie <brad@gizmoguy.net.nz>
2016-12-03 17:53:22 +1300

    Set CORS policy on /api. Update how we handle sameorigin on /dash and /taplist.

--- app/controllers/api/beers_controller.rb
+++ app/controllers/api/beers_controller.rb
@@ -1,4 +1,11 @@
 class Api::BeersController < ApiController
+	after_filter :cors_set_access_control_headers
+
+	def cors_set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'GET'
+		headers['Access-Control-Allow-Headers'] = '*'
+	end
 
 	def index
 		@beers = Beer.ontap
--- app/controllers/taplist_controller.rb
+++ app/controllers/taplist_controller.rb
@@ -1,8 +1,12 @@
 class TaplistController < ApplicationController
 	skip_before_filter :authenticate_user!
+	after_filter :sameorigin_unset_control_headers
+
+	def sameorigin_unset_control_headers
+		headers.delete "X-Frame-Options"
+	end
 
 	def index
-        response.headers.delete "X-Frame-Options"
 		@beers = Beer.joins(:location).where(locations: { status: ['LOW','SERVING'] }).order(:location_id)
 		respond_to do |format|
 			format.html { }
