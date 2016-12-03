351807e4153714841eff6444d3db5fdd519ab02b
Brad Cowie <brad@gizmoguy.net.nz>
2016-12-03 15:06:53 +1300

    Disable X-FRAME-OPTIONS for our dashboard to allow them to be iframeable.

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -1,6 +1,11 @@
 class DashController < ApplicationController
 	skip_before_filter :authenticate_user!
 
+    def my_embeddable_widget
+        response.headers.delete "X-Frame-Options"
+        render
+    end
+
 	def index
 		@beers = Beer.joins(:location).where(locations: { status: ['NEXT','LOW','SERVING'] }).order(:location_id)
 		respond_to do |format|
