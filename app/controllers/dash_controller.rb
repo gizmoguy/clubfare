9c5258753bf486ca23327335f55d4f89b5e7370e
Brad Cowie <brad@gizmoguy.net.nz>
2016-12-03 15:26:48 +1300

    Remove X-FRAME-OPTIONS from index action.

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -1,12 +1,8 @@
 class DashController < ApplicationController
 	skip_before_filter :authenticate_user!
 
-    def my_embeddable_widget
-        response.headers.delete "X-Frame-Options"
-        render
-    end
-
 	def index
+        response.headers.delete "X-Frame-Options"
 		@beers = Beer.joins(:location).where(locations: { status: ['NEXT','LOW','SERVING'] }).order(:location_id)
 		respond_to do |format|
 			format.html { }
